139c52e4885bd9549c571041748a3efd5e3996f3
Ishan_Khurz <khurjekar.ishan2136@gmail.com>
2017-02-26 05:57:39 +0000

    Part2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+        buffer = Array.new
+        self.select("rating").uniq.each {|y| buffer.push(y.rating)}
+        buffer.sort.uniq
+    end
 end
